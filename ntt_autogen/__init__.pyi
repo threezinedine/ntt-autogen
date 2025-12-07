cde1be14b77c901900358cdfa88b454bf527e5ec
threezinedine <threezinedine@gmail.com>
2025-12-07 22:53:14 +0700

    [bugfix] missing pyi file

---
+++ ntt_autogen/__init__.pyi
@@ -0,0 +1,9 @@
+from typing import Any
+
+class Autogen:
+    def __init__(
+        self,
+        tempFolder: str = "temp",
+        baseDir: str | None = None,
+        **kwargs: Any,
+    ) -> None: ...
